91fcdce48f3f51499cbdb9eb75f640f3d596c054
Suzy-Lee <>
2024-11-07 14:33:01 +0900

    feat: enum, any, unknown, void, naver 문법 학습

---
+++ onebite-typescript/section2/src/chapter6.ts
@@ -0,0 +1,15 @@
+// any
+// 특정 변수의 타입을 우리가 확실히 모를 때
+
+// unknown 
+// any와 마찬가지로 모든 타입을 대입할 수 있지만, 아무것도 못함. 연산 X, 메서드X
+let unknown: unknown;
+
+unknown = '';
+unknown = 1;
+unknown = () => {};
+
+
+
+
+
